11eb0e23b136662c5a6671a43550e432e0da8e66
Chandrashekar Dodali <146953506+chandu-uias@users.noreply.github.com>
2025-04-09 10:42:37 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -11,11 +11,21 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
+// app.use(cors({
+//   origin: ["http://localhost:3000"],
+//   methods: ["GET", "POST", "PUT", "DELETE"],
+//   credentials: true,
+// }));
+
 app.use(cors({
-  origin: ["http://localhost:3000"],
+  origin: [
+    "http://localhost:3000",
+    "https://nova-nine-umber.vercel.app"
+  ],
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true,
 }));
+
 app.use(bodyParser.json());
 app.use(express.json());
 
